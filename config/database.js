6dc8201347388647e12a874140086973e2aa23a4
Bhuvana Chandra P <bhuvanachandraprabhu@gmail.com>
2022-03-15 07:50:06 +0530

    added question route

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 
-const connectToDB=()=>{mongoose.connect('mongodb://localhost:27017/dbname',{useUnifiedTopology:true,useNewUrlParser:true},()=>{
+const connectToDB=()=>{mongoose.connect('mongodb://localhost:27017/SIH',{useUnifiedTopology:true,useNewUrlParser:true},()=>{
     console.log("Connected TO DB");
 });
 
