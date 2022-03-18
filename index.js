65d047bcc12a29d844096956f69c4753eed8d6cf
unknown <106120085@nitt.edu>
2022-03-18 16:45:12 +0530

    Added pdf Generation Route

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const connectToDB = require('./config/database');
 dotenv.config({path:'./config/config.env'});
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
 const apiRouter = require('./api/api');
 const authRouter = require('./api/auth');
+const pdfGenerate=require('./api/routes/pdfGenerateRoute');
 
 app.use('/api', apiRouter);
 app.use('/auth', authRouter);
+app.use('/api/v1',pdfGenerate);
 
 connectToDB();
 app.listen(process.env.PORT,()=>{
