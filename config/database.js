2045b50a2154cc7cdb614cc812f90eed628c8f72
Bhuvana Chandra P <bhuvanachandraprabhu@gmail.com>
2022-03-20 07:02:35 +0530

    added route for getting all assigned questions of a expert

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 
-const connectToDB=()=>{mongoose.connect('mongodb://localhost:27017/dbname',{useUnifiedTopology:true,useNewUrlParser:true},()=>{
+const connectToDB=()=>{mongoose.connect(`mongodb://localhost:27017/${process.env.DB_NAME}`,{useUnifiedTopology:true,useNewUrlParser:true},()=>{
     console.log("Connected TO DB");
 });
 
