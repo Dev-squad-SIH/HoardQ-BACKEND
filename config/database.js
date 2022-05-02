546ed900ebc6aa0f90d669b19a0a9613e36d5d4b
Santhosh <santhoshm12052003@gmail.com>
2022-05-02 19:58:59 +0530

    Edit example.env - Add Procfile for heroku deployment - Update file path

--- config/database.js
+++ config/database.js
@@ -4,7 +4,6 @@ const connectToDB = () => {
     mongoose.connect(process.env.DB_URL, { useUnifiedTopology: true, useNewUrlParser: true }, () => {
         console.log("Connected TO DB");
     });
-
 };
 
 module.exports = connectToDB;
